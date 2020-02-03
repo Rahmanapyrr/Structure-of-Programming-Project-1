e63c5ffe4e5623ad4c599b0ff87fca1787ed757e
Rahmanapyrr <32530384+Rahmanapyrr@users.noreply.github.com>
2020-02-03 11:41:35 -0500

    trying this out

--- csci350.scm
+++ csci350.scm
@@ -1,1 +1,12 @@
 // starting my structures assignment
+
+(define (deep-reverse L)
+  (cond ((null? L) '())
+        ((list? L)
+         (append (deep-reverse (cdr L))
+                 (list (deep-reverse (car L)))
+                 )
+         )
+        (else L)
+        )
+  )
