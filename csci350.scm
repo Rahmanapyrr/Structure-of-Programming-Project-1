00ac309590aa3b7aae8c584c32087d0ced52e51a
Rahmanapyrr <32530384+Rahmanapyrr@users.noreply.github.com>
2020-02-04 22:41:05 -0500

    Update csci350.scm

--- csci350.scm
+++ csci350.scm
@@ -1,4 +1,3 @@
-// starting my structures assignment
 
 (define (deep-reverse L)
   (cond ((null? L) '())
@@ -9,4 +8,14 @@
          )
         (else L)
         )
+  )
+
+(define (sum-up-numbers-simple L)
+    (cond ((null? L) 0)
+          ((list? L)
+           (if (number? (car L))
+               (+ (sum-up-numbers-simple (cdr L)) (car L))
+           )
+          (else 0)
+          )
   )
